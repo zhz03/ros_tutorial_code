c1cab1b876ba93f12b7720c62eb91f7923907175
Zhaoliang <zhz03@g.ucla.edu>
2023-04-19 13:36:05 -0700

    upload 01_ros_basics

---
+++ 01_ros_basics/hello_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import ros environment
+import rospy
+
+if __name__ == '__main__':
+    # initialize ros node, need to specify node name
+    rospy.init_node('itcast_node')
+
+    # rate , 10 hz: 10 times per second
+    rate = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        print('hello ros python')
+        rate.sleep()
+
+    rospy.spin()
